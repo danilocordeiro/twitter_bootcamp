bc64e03a0449d3bf0b749b0ef9a3b228320e0900
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-08-19 13:12:08 -0300

    Relacionamentos de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,31 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tweets
+
+  has_many :active_relationships,  class_name: 'Relationship',
+           foreign_key: 'follower_id', dependent: :destroy
+
+  has_many :passive_relationships, class_name: 'Relationship',
+            foreign_key: 'followed_id', dependent: :destroy
+
+  has_many :following, through: :active_relationships,  source: :followed
+  
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def following?(other_user)
+    # Este método checa se um usuário está seguindo o outro.
+  end
+
+  def follow!(other_user)
+    # Este método criará o relacionamento entre um usuário e outro.
+  end
+
+  def unfollow!(other_user)
+    # Este método apagará o relacionamento entre um usuário e outro.
+  end
+
+  def feed
+    # Este método irá gerar o feed para o usuário
+    Tweet.where(user_id: followers.pluck(:id)).order(created_at: :desc)
+  end
 end
